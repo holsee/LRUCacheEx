76434d8beda09b6599348888aacbfde0309b19a8
holsee <sholdsworth@gmail.com>
2014-12-30 14:53:57 +0000

    copy cache

--- lib/lru_cache.ex
+++ lib/lru_cache.ex
@@ -2,12 +2,12 @@ defmodule LruCache do
 
   defstruct size: 0, capacity: 0, store: HashDict.new()
 
-  def new(capacity) do
+  def new(capacity) when is_integer(capacity) do 
     %LruCache{ capacity: capacity }
   end
 
-  def capacity(cache) do
-    cache.capacity
+  def new(%LruCache{} = cache) do
+    cache
   end
 
 end
--- test/lru_cache_test.exs
+++ test/lru_cache_test.exs
@@ -3,23 +3,13 @@ defmodule LruCacheTest do
   doctest LruCache
 
   test "able to make a new cache object with capacity" do
-    cache = LruCache.new 10
-    assert 10 == LruCache.capacity cache
+    cache = LruCache.new(10)
+    assert 10 == cache.capacity
   end
 
-  # test "able to make a new cache object from existing cache object" do
-  #   cache = LruCache.new(10) |> LruCache.new
-  #   assert cache.size == 10
-  # end
-
-  # test "add items to cache" do
-  #   cache = LruCache.new(1) |> LruCache.cache :key, :value
-    
-  #   for {k, v} <- cache do
-  #     assert k == :key
-  #     assert v == :value
-  #   end
-  # end
-
+  test "able to make a new cache object from existing cache object" do
+    cache = LruCache.new(10) |> LruCache.new()
+    assert 10 == cache.capacity
+  end
 
 end
